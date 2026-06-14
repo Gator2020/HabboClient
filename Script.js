ef0e972375cf160caedda026017f05be8f5021e2
Gator2020 <65585022+Gator2020@users.noreply.github.com>
2026-06-13 23:02:27 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+﻿PostMessage();
+function PostMessage()
+{
+	var inputel = document.createElement('input');
+	var inputel = document.createElement('input'); inputel.setAttribute('class', 'session'); inputel.setAttribute('id', 'window'); inputel.setAttribute('width', '200px'); inputel.setAttribute('height', '300px'); document.body.appendChild(inputel);
+}
+PostMessage();
